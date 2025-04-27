2d005b580ade7a7c871691db70bf512dc3ae4b6d
samipevekar <samipevekar499@gmail.com>
2025-04-27 17:55:14 +0530

    add hosted url of pg

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,30 +1,30 @@
-// import pg from 'pg'
-// const { Pool } = pg
-
-// const pool = new Pool({
-//     connectionString:"postgresql://snjl_liquors_user:t84LyziEDIDyU8scj3cEF29ZMqopT9iV@dpg-cv44esbqf0us73b5fuv0-a.oregon-postgres.render.com/snjl_liquors",
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// })
-
-// pool.connect()
-//   .then(() => console.log("Connected to PostgreSQL Database"))
-//   .catch(err => console.error("Connection Error:", err));
-
-import pg from 'pg';
-const { Pool } = pg;
+import pg from 'pg'
+const { Pool } = pg
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'snjl_local',
-  password: '7901sami', // 🛑 Replace with your actual password
-  port: 5432,
-});
+    connectionString:"postgresql://ems_snjl_user:owOat8AXiEVJb0yJc7vSD4OFx3dN8VXz@dpg-d071ruili9vc73erqdag-a.oregon-postgres.render.com/ems_snjl",
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
 pool.connect()
-  .then(() => console.log("✅ Connected to Local PostgreSQL Database"))
-  .catch(err => console.error("❌ Local Connection Error:", err));
+  .then(() => console.log("Connected to PostgreSQL Database"))
+  .catch(err => console.error("Connection Error:", err));
+
+// import pg from 'pg';
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'snjl_local',
+//   password: '7901sami', // 🛑 Replace with your actual password
+//   port: 5432,
+// });
+
+// pool.connect()
+//   .then(() => console.log("✅ Connected to Local PostgreSQL Database"))
+//   .catch(err => console.error("❌ Local Connection Error:", err));
 
 export const query = (text, params) => pool.query(text, params);
